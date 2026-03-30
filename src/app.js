125c394dcdcdc12395b03c2e35325fee02048963
captainJosh-1 <chetanoffi.23@gmail.com>
2026-03-31 01:53:31 +0530

    utils code

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+
+//data from any form or something
+app.use(express.json({limit: "16kb"}))
+// const limit = "16kb"; we can use this too like creating constant just to enhence the lv of our code 
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
+//pdf and images 
+app.use(express.static("public"))
+
+
+app.use(cookieParser())
+
+
+export {app}
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,39 @@
+//by promises
+const asyncHandler = (requestHandler) => {
+    (req , res , next) =>{
+        Promise.resolve(requestHandler(req , res , next )).
+        catch((err) => next(err))
+    }
+}
+
+
+
+export {asyncHandler}
+
+
+
+
+
+
+
+
+
+
+
+//by try and catch
+// const asyncHandler= () => {}
+// const asyncHandler= (func) => () =>{}
+// const asyncHandler= (func) => async () =>{}
+
+
+// const asyncHandler = (fn)=> async (req,res,next) => {
+//     try{
+//         await fn(req, res , next)
+
+//     } catch(error){
+//         res.status(error.code || 500).json({
+//             success:false,
+//             message:error.message
+//         })
+//     }
+// }
